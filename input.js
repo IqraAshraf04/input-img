4ca1a4084e825438f8c2195adeebd8df1575093f
Iqra <iwrites04@gmail.com>
2022-06-07 10:34:32 +0500

    updated

--- input.js
+++ input.js
@@ -1,7 +1,25 @@
-var loadFile = function(event) {
-    var output = document.getElementById('output');
-    output.src = URL.createObjectURL(event.target.files[0]);
-    output.onload = function() {
-        URL.revokeObjectURL(output.src) // free memory
+function previewImages() {
+    var preview = document.querySelector('#preview')
+
+    if (this.files) {
+        [].forEach.call(this.files, readAndPreview)
     }
-};
+    function readAndPreview(file) {
+        // Make sure 'file.name' matches our extensions criteria
+        if (!/\.(jpe?g|png|gif)$/i.test(file.name)) {
+            return alert(file.name + "is not an image");
+        } // else...
+        var reader = new FileReader();
+
+        reader.addEventListener("load", function() {
+            var image = new Image ();
+            image.height = 100;
+            image.title = file.name;
+            image.src =   this.result;
+            preview.appendChild(image);
+        });
+        reader.readAsDataURL(file);
+    }
+    
+}
+document.querySelector('#file-input').addEventListener("change", previewImages);
